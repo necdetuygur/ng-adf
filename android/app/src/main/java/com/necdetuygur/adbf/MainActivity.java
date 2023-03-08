f01252bedd87b6b08aad13b39bdf2704ac7571f1
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-08 15:49:19 +0300

    Capacitor: Mobile application

---
+++ android/app/src/main/java/com/necdetuygur/adbf/MainActivity.java
@@ -0,0 +1,5 @@
+package com.necdetuygur.adbf;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.necdetuygur.adbf',
+  appName: 'Altın Döviz Borsa Fiyatları',
+  webDir: 'dist/ng-adf',
+  bundledWebRuntime: false,
+};
+
+export default config;
