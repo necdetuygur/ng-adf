d936bd1ed8e23deab0e45f08b0f4cb9931755011
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-08 14:58:35 +0300

    Hisse senetleri portföyü içe & dışa aktar yapıldı.

--- src/app/hisse/hisse.component.ts
+++ src/app/hisse/hisse.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import axios from 'axios';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-hisse',
@@ -10,7 +11,11 @@ export class HisseComponent {
   loading = false;
   hisseKodu = '';
   hisses = JSON.parse('' + localStorage.getItem('Hisse')) || [];
+  aktar = 'Dışa';
+  modalAktar: any;
+  aktarData = '';
   ngOnInit() {
+    this.modalAktar = new bootstrap.Modal('#modalAktar');
     this.fiyatlariGetir();
   }
   fiyatlariGetir() {
@@ -21,7 +26,7 @@ export class HisseComponent {
     });
     Promise.all(gets).then((datas: any[]) => {
       this.hisses.forEach((hisse: any, index: number) => {
-        const data = ('' + datas[index].data).replace('.', ',').split("|");
+        const data = ('' + datas[index].data).replace('.', ',').split('|');
         hisse.fiyat = data[0];
         hisse.yuzde = data[1];
       });
@@ -41,9 +46,21 @@ export class HisseComponent {
       this.hisseKodu = '';
       return;
     }
+    this.hissesAdd(this.hisseKodu);
+    this.hisseKodu = '';
+    this.fiyatlariGetir();
+  }
+  hissesAdd(hk: string) {
+    if (
+      this.hisses.filter((hisse: any) => hisse.kod === hk).length > 0 ||
+      hk.length < 3
+    ) {
+      return;
+    }
+
     this.hisses.push({
       id: new Date().getTime(),
-      kod: this.hisseKodu,
+      kod: hk,
       fiyat: '0,0',
       yuzde: '0,0%',
     });
@@ -53,12 +70,42 @@ export class HisseComponent {
       return 0;
     });
     localStorage.setItem('Hisse', JSON.stringify(this.hisses));
-    this.hisseKodu = '';
-    this.fiyatlariGetir();
   }
   sil(id: number) {
     if (!confirm('Emin misiniz?')) return;
     this.hisses = this.hisses.filter((hisse: any) => hisse.id != id);
     localStorage.setItem('Hisse', JSON.stringify(this.hisses));
+  }
+  disaAktar() {
+    this.aktarData = this.hisses
+      .map((hisse: any) => hisse.kod)
+      .join('\n')
+      .trim();
+    this.aktar = 'Dışa';
+    this.modalAktar.show();
+  }
+  iceAktar() {
+    this.aktarData = '';
+    this.aktar = 'İçe';
+    this.modalAktar.show();
+  }
+  aktarDataKopyala() {
+    const aktarDataTxt =
+      document.querySelector<HTMLTextAreaElement>('#aktarDataTxt');
+    aktarDataTxt?.focus();
+    aktarDataTxt?.select();
+    document.execCommand('copy');
+  }
+  iceAktarmaBaslat() {
+    const aktarDataTxt =
+      document.querySelector<HTMLTextAreaElement>('#aktarDataTxt');
+    aktarDataTxt?.value
+      .trim()
+      .split('\n')
+      .forEach((hk: string) => {
+        this.hissesAdd(hk.trim().toUpperCase());
+      });
+    this.fiyatlariGetir();
+    this.modalAktar.hide();
   }
 }
